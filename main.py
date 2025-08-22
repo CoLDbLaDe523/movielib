266a98fc6a7bc9d1f7f7d06ad2679d064ff108b4
User <and.pak993@gmail.com>
2025-08-22 18:44:12 +0500

    commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from database import models
 from database.data_base import engine
 from api import auth_api, movies_api, favorites_api, ratings_api, stats_api
 from fastapi.templating import Jinja2Templates
 from fastapi.security import OAuth2PasswordBearer
-
+from fastapi.responses import HTMLResponse
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -19,5 +19,10 @@ app.include_router(movies_api.router, prefix="/movies", tags=["Movies"])
 app.include_router(favorites_api.router, prefix="/favorites", tags=["Favorites"])
 app.include_router(ratings_api.router, prefix="/ratings", tags=["Ratings"])
 app.include_router(stats_api.router, prefix="/stats", tags=["Stats"])
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
